92caaa168dafdd85f65e985ae831bd6a4af44bb1
RichertFeynstein <Richert.feynstein.edu1@gmail.com>
2023-08-31 01:56:09 +0530

    adding first changes to simple chat app

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('send_message', (message) => {
+    io.emit('receive_message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
