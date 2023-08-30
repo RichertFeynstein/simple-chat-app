9a1d988e8f3725e3775aedb246d7b53b6b6bb4ff
RichertFeynstein <Richert.feynstein.edu1@gmail.com>
2023-08-31 02:05:07 +0530

    Made changes to this file on how static html pages are added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 
@@ -6,13 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
 io.on('connection', (socket) => {
   console.log('New client connected');
-
   socket.on('send_message', (message) => {
     io.emit('receive_message', message);
   });
-
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
